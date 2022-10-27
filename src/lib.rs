f6c34168db35f47f737d77446b2f34fd27e99ffc
C.W. Chien <dppss92132@gmail.com>
2022-10-27 18:33:01 +0800

    Test reqwest GET mothod
    Can not compiled yet. May transfer unit test to integration test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use reqwest;
+use std::collections::HashMap;
+
+struct header {}
+
+fn initialize() {}
+
+pub fn get_note(note_id: String) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+    let mut note =
+        reqwest::blocking::get("https://httpbin.org/ip")?.json::<HashMap<String, String>>()?;
+    if note.len() == 1 {
+        Ok(note)
+    } else {
+        Err("Bad Request".into())
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +20,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_works() -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+        let result = get_note(String::from("7n8KZy0oQDKQrhwCOhi0KQ"));
+        println!("{:#?}", result); // std out seems useless in unit test but I don't neet assert here
+        Ok(result.expect("It may panic")) // test will fail if got Err in get_note
     }
 }
